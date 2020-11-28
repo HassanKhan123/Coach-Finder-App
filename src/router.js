1c6afddb581edca4cf5a56b4e9e40b822fe3f54b
HassanKhan123 <hk2602809@gmail.com>
2020-11-28 12:45:10 +0500

    login and signup page added

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import CoachRegistration from '@/pages/coaches/CoachRegistration';
 import ContactCoach from '@/pages/requests/ContactCoach';
 import RequestsReceived from '@/pages/requests/RequestsReceived';
 import NotFound from './pages/NotFound.vue';
+import UserAuth from './pages/auth/UserAuth.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -33,6 +34,10 @@ const router = createRouter({
     {
       path: '/requests',
       component: RequestsReceived
+    },
+    {
+      path: '/auth',
+      component: UserAuth
     },
     {
       path: '/:notFound(.*)',
