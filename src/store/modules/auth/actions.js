a8fbbdb41c7410712d0f1d93bb1cd264a2c6c4cc
HassanKhan123 <hk2602809@gmail.com>
2020-11-28 14:52:36 +0500

    login action added

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,5 +1,28 @@
 export default {
-  login() {},
+  async login(context, payload) {
+    const res = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBMLbBBbNyt28fqSEAtRzDmKqH8noA9hqs',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true
+        })
+      }
+    );
+    const resData = await res.json();
+    if (!res.ok) {
+      const error = new Error(resData.message || 'Failed to authenticate.');
+      throw error;
+    }
+    console.log(resData);
+    context.commit('setUser', {
+      token: resData.idToken,
+      tokenExpiration: resData.expiresIn,
+      userId: resData.localId
+    });
+  },
   async signup(context, payload) {
     const res = await fetch(
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBMLbBBbNyt28fqSEAtRzDmKqH8noA9hqs',
