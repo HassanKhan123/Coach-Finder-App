16fae6663ddc0f1eb37d320e537917dc460b35b2
HassanKhan123 <hk2602809@gmail.com>
2020-11-28 16:31:37 +0500

    auto logout functionality added

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,3 +1,5 @@
+let timer;
+
 export default {
   async login(context, payload) {
     return context.dispatch('auth', {
@@ -12,10 +14,15 @@ export default {
     });
   },
   logout(context) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('expiration');
+
+    clearTimeout(timer);
+
     context.commit('setUser', {
       userId: null,
-      token: null,
-      tokenExpiration: null
+      token: null
     });
   },
   async auth(context, payload) {
@@ -39,24 +46,45 @@ export default {
       const error = new Error(resData.message || 'Failed to authenticate.');
       throw error;
     }
+    const expiresIn = +resData.expiresIn * 1000;
+    const expirationDate = new Date().getTime() + expiresIn;
     localStorage.setItem('token', resData.idToken);
     localStorage.setItem('userId', resData.localId);
+    localStorage.setItem('expiration', expirationDate);
+
+    timer = setTimeout(() => {
+      context.dispatch('autoLogout');
+    }, expiresIn);
+
     context.commit('setUser', {
       token: resData.idToken,
-      tokenExpiration: resData.expiresIn,
       userId: resData.localId
     });
   },
   autoLogin(context) {
     const token = localStorage.getItem('token');
     const userId = localStorage.getItem('userId');
+    const tokenExpiration = localStorage.getItem('expiration');
+
+    const expiresIn = +tokenExpiration - new Date().getTime();
+
+    if (expiresIn < 0) {
+      return;
+    }
+
+    timer = setTimeout(() => {
+      context.dispatch('autoLogout');
+    }, expiresIn);
 
     if (token && userId) {
       context.commit('setUser', {
         token,
-        userId,
-        tokenExpiration: null
+        userId
       });
     }
+  },
+  autoLogout(context) {
+    context.dispatch('logout');
+    context.commit('didAutoLogout');
   }
 };
